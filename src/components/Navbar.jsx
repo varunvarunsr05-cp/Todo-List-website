fef91f537167324e14963ebeb24edd9419fe52f6
varunvarunsr05-cp <varunvarunsr05@gmail.com>
2026-07-12 18:09:01 +0530

    navbar fixing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <nav className='bg-purple-600 text-white flex   py-2.5 justify-between md:justify-around'>
       <div className="logo  font-extrabold  text-xl ">iTask</div>
       <ul  className='flex gap-5'>
-        <li className='hover:font-bold transition-all text-lg ' >Home</li>
+        <li className='hover:font-bold transition-all text-lg mx-2.5' >Home</li>
         <li className='hover:font-bold transition-all text-lg mx-2.5' >Your Tasks</li>
       </ul>
     </nav>
